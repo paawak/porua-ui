db4f17c2419502828a8371078065d0e69fe904e5
Palash Ray <paawak@gmail.com>
2020-11-26 19:06:46 +0530

    put text in login

--- src/GoogleSignInComponent.jsx
+++ src/GoogleSignInComponent.jsx
@@ -6,22 +6,24 @@ const CLIENT_ID = '955630342713-55eu6b3k5hmsg8grojjmk8mj1gi47g37.apps.googleuser
 
 
 class GoogleSignInComponent extends Component {
-   constructor(props) {
-    super(props);    
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
-    <div>
-      <GoogleLogin
-          clientId={ CLIENT_ID }
-          buttonText='Login'
-          onSuccess={ this.props.loginSuccess }
-          onFailure={ this.props.handleLoginFailure }
-          cookiePolicy={ 'single_host_origin' }
+      <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal">Welcome to Porua: A Bengali OCR Correction Platform</h1>
+        <p>Sign In</p>
+        <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText='Google'
+          onSuccess={this.props.loginSuccess}
+          onFailure={this.props.handleLoginFailure}
+          cookiePolicy={'single_host_origin'}
           responseType='code,token'
         />
-    </div>
+      </div>
     )
   }
 }
