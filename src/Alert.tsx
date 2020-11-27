96c8384d725632fb07341523b922f3a897cfaec0
Palash Ray <paawak@gmail.com>
2020-11-27 19:43:36 +0530

    bug fix

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -17,19 +17,27 @@ class Alert extends Component<AlertProps, AlertState> {
 
   render() {
     let className = "alert alert-dismissible fade show";
-    let message;
 
     if (this.props.alertType === AlertType.SUCCESS) {
       className += " alert-success";
-      message = "<strong>Success!</strong> " + this.props.message + " operation was succesful!";
     } else if (this.props.alertType === AlertType.ERROR) {
       className += " alert-danger";
-      message = "<strong>Error in " + this.props.message + " operation!</strong> Please try again."
     }
 
     return (
       <div className={className} role="alert">
-        {message}
+
+        {(() => {
+          if (this.props.alertType === AlertType.SUCCESS) {
+            return <><strong>Success!</strong>{this.props.message} operation was succesful!</>
+          } else if (this.props.alertType === AlertType.ERROR) {
+            return <><strong>Error in {this.props.message} operation!</strong> Please try again.</>
+          } else {
+            return <strong>{this.props.message}</strong>
+          }
+        })()
+        }
+
         <button type="button" className="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
